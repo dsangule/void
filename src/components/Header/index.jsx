26faa7d0742af2f76ca001dcac7e9c854205e196
dsangule <dsangule@gmail.com>
2024-02-24 21:39:50 +0530

    First commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="bg-violet-700 h-12 w-full">
+            <h1 className="text-white">Header</h1>
+        </div>
+    )
+}
---
+++ src/components/LandingPage/index.jsx
@@ -0,0 +1,10 @@
+import Header from "../Header";
+
+export default function LandingPage() {
+    return (
+        <div className="min-h-screen bg-black">
+            <Header />
+            <h1 className="text-white">Landing Page</h1>
+        </div>
+    )
+}
